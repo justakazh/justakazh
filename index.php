97c64b2b896f9ce363855db1b0879e063a0ef0f1
AKAZH ID <36891477+justakazh@users.noreply.github.com>
2019-11-22 12:45:05 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,112 @@
+<!doctype html>
+<html lang="en">
+<head>
+<!-- Required meta tags -->
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+<!-- Bootstrap CSS -->
+<link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+<link rel="stylesheet" href="css/home.css">
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+
+
+<title>Akas Wisnu Aji</title>
+</head>
+<body>
+<!-- Image and text -->
+<nav class="navbar">
+    <div class="container">
+        <a class="navbar-brand" href="#">
+            Akas Wisnu Aji
+        </a>
+        <ul class="nav justify-content-end">
+            <li class="nav-item">
+                <a class="nav-link active" href="#">Home</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Github</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Facebook</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Instagram</a>
+            </li>
+        </ul>
+    </div>
+</nav>
+
+
+<div class="jumbotron jumbotron-fluid">
+    <div class="container">
+        <img src="https://figuya.com/uploads/product/profile_picture/16691/profile_dragon-ball-super-handtuch-super-saiyajin-god-super-saiyajin-son-goku-150-x-75-cm20190808-19143-53pua7.jpeg" alt="..." class="img-thumbnail rounded-circle mx-auto d-block">
+        <br>
+        <h4 class="text-center"><u>Akas Wisnu Aji</u> </h4>
+        <br>
+        <p class="text-center">
+            Student | Coder | Pentester | Wibu | Gamer </p>
+    </div>
+    </div>
+
+
+<!-- section 1 -->
+<section class="section1">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <h1 class="text-center">Hi Everyone!</h1>
+                <hr style="width:200px">
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-6">
+                <h4 class="text-center">English</h4>
+                <hr style="width: 200px">
+                <p>
+                    Welcome to my simple portfolio. My name is <b>Akas Wisnu Aji</b>, I am 17 years old, I was born in Bekasi June 28 2002. I am a student in Central Java. my hobby is playing computer and learning about new technology, I also like watching anime 
+                    
+                </p>
+            </div>
+            <div class="col-md-6">
+                    <h4 class="text-center">Indonesia</h4>
+                <hr style="width: 200px">
+                <p>
+                        Selamat datang di portfolio simpel saya. Nama saya adalah <b>Akas Wisnu Aji</b>, umur saya 17 tahun, saya lahir di bekasi 28 juni 2002. saya adalah seorang pelajar di jawa tengah. hobi saya bermain komputer dan mempelajari seputar teknologi baru, saya juga suka menonton anime 
+                </p>
+            </div>
+        </div>
+    </div>
+</section>
+
+<!-- section2 -->
+<section class="section2">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <h1 class="text-center">Gallery</h1>
+                <hr style="width: 200px">
+            </div>
+        </div>
+        <div class="row">
+                <div class="col-md-12">
+                    <!-- AIzaSyC4wD1Fm509i37i-nnxrrWR9ffe_tzGaaQ -->
+                </div>
+            </div>
+    </div>
+</section>
+
+
+
+    <script>
+        $(document).ready(functoin(){
+            alert("oke")
+        });
+    </script>
+<!-- Optional JavaScript -->
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<script src="assets/bootstrap/js/jquery.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="assets/bootstrap/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
+</html>
